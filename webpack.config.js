bcebe8e44b0583df53737b0db1ab34ab8cd6847b
Tobias Bazzan <35808410+eifachnurtobi@users.noreply.github.com>
2023-02-15 09:21:59 +0100

    Update webpack config, rebuild.

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         use: [
           {
             loader: 'babel-loader',
-            query: {
+            options: {
               presets: [ '@babel/preset-env' ],
             },
           },
